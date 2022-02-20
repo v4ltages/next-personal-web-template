d14dce27c520c09234ae5daf64bdd5b8da7ec812
v4ltages <contact@voltages.me>
2022-02-21 01:49:23 +0200

    style: still needs more work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Head>
+      <meta charSet="utf-8"/>
+      <meta name="description" content="Personal website template made by Voltages"/>
+      <meta name="author" content="v4ltages"/>
+      <meta name="keywords" content="template, personal, website, add, more, here"/>
+      <meta name="viewport" content="width=device-width,initial-scale=1"/>
+  </Head>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,31 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { motion } from "framer-motion";
+import { Footer } from "../components/Footer";
 import { GitHubButton } from "../components/GitHubButton";
+import profilePic from '/public/avatar.png'
 
 const Home: NextPage = () => {
     return (
         <>
             <Head>
-                <title>voltages&apos;s next.js template</title>
+                <title>voltages&apos;s next.js personal website template</title>
             </Head>
             <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center text-white text-3xl font-semibold">
-                voltages&apos;s next.js template
-                <GitHubButton />
+                <div className="p-8 flex">
+                    <img src={profilePic.src} alt="avatar" className="rounded-2xl w-32 h-32"/>
+                    <motion.section animate={{ opacity: [0, 1], x: [-20, 0] }} className="pl-8">
+                        <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-[#D8B7EE] via-[#E494AC] to-[#A1CBF7]">Hello there.</h1>
+                        <p className="font-normal text-lg">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis atque libero tempora?
+                        </p>
+                        <div className="flex flex-row gap-4">
+                            <GitHubButton />
+                        </div>
+                    </motion.section>
+                </div>
             </div>
+            <Footer/>
         </>
     );
 };
